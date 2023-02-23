316ead6bd35ef2cff2e9d9f5fb6b70222fa57295
JonnyBoy04 <simonzdjgtz@gmail.com>
2023-02-22 23:26:36 -0600

    Esta es mi primer versión

---
+++ HolaGit/src/holagit/HolaGit.java
@@ -0,0 +1,15 @@
+package holagit;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class HolaGit {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola, Mundo!");
+    }
+    
+}
